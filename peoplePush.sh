bf8b4b4d626b8e54459bca5fde9de03f62b04c8d
Valentin Tsatskin <vtsatskin@mozilla.com>
2014-06-20 14:28:59 -0700

    people pushing public parts

--- peoplePush.sh
+++ peoplePush.sh
@@ -8,3 +8,10 @@ wintersmith build
 # Repalce occurances of /images with images in generated html files. fuck sed.
 sed -i '' 's/\/images/images/g' build/*.html
 rsync -avz build/ people:public_html/secure/SmartData/
+
+# Upload public files
+rsync -avz build/css people:public_html/specs/SmartData/
+rsync -avz build/js people:public_html/specs/SmartData/
+rsync -avz build/images/logo.svg people:public_html/specs/SmartData/images/
+rsync -avz build/images/specs people:public_html/specs/SmartData/images/
+rsync -avz build/specs.html people:public_html/specs/SmartData/index.html
