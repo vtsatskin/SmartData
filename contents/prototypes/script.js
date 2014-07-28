2933cbfeefe0194ab9537d751f510b69da6194e7
Valentin Tsatskin <vtsatskin@mozilla.com>
2014-07-28 13:34:09 -0700

    dynamically generated usage bars

--- contents/prototypes/script.js
+++ contents/prototypes/script.js
@@ -1,16 +1,50 @@
 (function() {
-  var forEach = Array.prototype.forEach;
+  var data = {
+    apps: [
+      {
+        title: "Browser",
+        icon: "browser_84.png",
+        usage: "250",
+        useMobileData: true
+      },
+      {
+        title: "Music",
+        icon: "music_84.png",
+        usage: "145",
+        useMobileData: true,
+      },
+      {
+        title: "Email",
+        icon: "email_84.png",
+        usage: "105",
+        useMobileData: true
+      }
+    ],
+    maxUsage: 1000
+  };
 
-  var usageBars = document.querySelectorAll(".usage-bar");
-  forEach.call(usageBars, function(bar) {
-    bar.addEventListener('click', function() {
-      document.querySelector('#application-usage').className = 'current';
-      document.querySelector('[data-position="current"]').className = 'left';
-    });
+  var source   = $("#usage-bars-template").html();
+  var template = Handlebars.compile(source);
+  $("#usage-bars").html(template(data));
+
+  source = $("#application-usage-template").html();
+  var appUsageTemplate = Handlebars.compile(source);
+
+  $("#usage-bars .usage-bar").each(function(index) {
+    $(this).click(usageBarClickEventGenerator(index));
   });
 
-  document.querySelector("#application-usage-back").addEventListener("click", function() {
-    document.querySelector('#application-usage').className = 'right';
-    document.querySelector('[data-position="current"]').className = 'current';
-  });
+  function usageBarClickEventGenerator(index) {
+    return function() {
+      $('#application-usage')[0].className = 'current';
+      $('[data-position="current"]')[0].className = 'left';
+
+      $("#application-usage").html(appUsageTemplate(data.apps[index]));
+
+      $("#application-usage-back").click(function() {
+        $('#application-usage')[0].className = 'right';
+        $('[data-position="current"]')[0].className = 'current';
+      });
+    }
+  }
 })();
