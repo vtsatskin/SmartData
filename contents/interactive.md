8396a0d385ee22af251db453bfdaabf66ce06928
Valentin Tsatskin <vtsatskin@mozilla.com>
2014-07-28 14:50:07 -0700

    add multi-SIM

--- contents/interactive.md
+++ contents/interactive.md
@@ -6,3 +6,5 @@ template: index.jade
 ## Example
 
 <iframe class="prototype prototype-fxos" src="prototypes/index.html"></iframe>
+
+<iframe class="prototype prototype-fxos" src="prototypes/index-multi.html#SIM1"></iframe>
--- contents/prototypes/script.js
+++ contents/prototypes/script.js
@@ -1,5 +1,6 @@
 (function() {
-  var data = {
+  var data1 = {
+    simID: "SIM1",
     apps: [
       {
         title: "Browser",
@@ -20,26 +21,72 @@
         useMobileData: true
       }
     ],
-    maxUsage: 1000
+    usageLimit: {
+      value: 1000,
+      unit: "MB"
+    },
+    usage: {
+      value: 500,
+      unit: "MB",
+      progress: 0.5
+    }
   };
 
-  var source   = $("#usage-bars-template").html();
-  var template = Handlebars.compile(source);
-  $("#usage-bars").html(template(data));
+  var data2 = {
+    simID: "SIM2",
+    apps: [
+      {
+        title: "Browser",
+        icon: "browser_84.png",
+        usage: "100",
+        useMobileData: true
+      },
+      {
+        title: "Email",
+        icon: "email_84.png",
+        usage: "50",
+        useMobileData: true
+      },
+      {
+        title: "Music",
+        icon: "music_84.png",
+        usage: "10",
+        useMobileData: true,
+      },
+    ],
+    usageLimit: {
+      value: 256,
+      unit: "MB"
+    },
+    usage: {
+      value: 160,
+      unit: "MB",
+      progress: 0.625
+    }
+  };
 
   source = $("#application-usage-template").html();
   var appUsageTemplate = Handlebars.compile(source);
 
-  $("#usage-bars .usage-bar").each(function(index) {
-    $(this).click(usageBarClickEventGenerator(index));
-  });
+  function renderTotalUsage(data) {
+    return function() {
+      var source   = $("#total-usage-template").html();
+      var template = Handlebars.compile(source);
+      $("#total-usage-container").html(template(data));
 
-  function usageBarClickEventGenerator(index) {
+      $("#usage-bars .usage-bar").each(function(index) {
+        $(this).click(usageBarClickEventGenerator(data, index));
+      });
+    }
+  }
+
+  function usageBarClickEventGenerator(data, index) {
     return function() {
       $('#application-usage')[0].className = 'current';
       $('[data-position="current"]')[0].className = 'left';
 
       $("#application-usage").html(appUsageTemplate(data.apps[index]));
+      $("#application-usage-back").attr("href", "#" + data.simID);
 
       $("#application-usage-back").click(function() {
         $('#application-usage')[0].className = 'right';
@@ -47,4 +94,13 @@
       });
     }
   }
+
+  renderTotalUsage(data1)();
+
+  //
+  // Mulit-SIM
+  //
+
+  $("#SIM1").click(renderTotalUsage(data1));
+  $("#SIM2").click(renderTotalUsage(data2));
 })();
